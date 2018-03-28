da934512cf870621d164642f68cefbe435386df9
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-03-28 13:53:37 +0100

    populate done but not tested

--- populate.py
+++ populate.py
@@ -1,6 +1,34 @@
 import os
 
 
+def populate():
+    u1 = add_user(username='arnold', password='hasapassword')
+    add_user_profile(u1, 0, 4000, "Los Angeles")
+    u2 = add_user(username='harold', password='hasapassword')
+    add_user_profile(u2, 0, 900, "Moscow")
+    u3 = add_user(username='john', password='hasapassword')
+    add_user_profile(u2, 10.1, 765, "Cape Town")
+
+    w1 = add_workout_type('deadlift', False)
+    add_workout_type('squat', False)
+    w2 = add_workout_type('pushup', False)
+    add_workout_type('curl', False)
+
+    add_workout_type('rowing', True)
+    add_workout_type('cycling', True)
+    add_workout_type('running', True)
+    w3 = add_workout_type('walking', True)
+
+    add_workout(u1, w1, 10, 4, 100, 0, 0)
+    add_workout(u2, w2, 20, 3, 15, 0, 0)
+    add_workout(u3, w3, 0, 0, 0, 10, 0.01)
+    add_workout(u3, w1, 15, 3, 17, 0, 0)
+
+    add_team([u1, u2], "Elderly", "Elderly")
+    add_team([u1, u3], "Buff", "Buff")
+    add_team([u2], "LonelyPeople", "Lonely People")
+
+
 def add_team(users, team_id, name):
     t = Team.objects.get_or_create(team_id=team_id, name=name)
     for u in users:
@@ -24,7 +52,7 @@ def add_workout_type(name, cardio):
 
 
 def add_user_profile(user, dp, wp, location):
-    u_p = UserProfile.objects.get_or_create(user=user, distancepoints=dp, weightpoints=dp, location=location)
+    u_p = UserProfile.objects.get_or_create(user=user, distancepoints=dp, weightpoints=wp, location=location)
     return u_p
 
 
@@ -37,3 +65,5 @@ if __name__ == '__main__':
     print("Starting database population!")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wwworkout.settings')
     from rango.models import *
+
+    populate()
